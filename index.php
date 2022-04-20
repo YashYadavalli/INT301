68c308a3c7aa9f87ec39a73c4d0786c42f642b4f
YashYadavalli <93912412+YashYadavalli@users.noreply.github.com>
2022-04-21 00:07:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,220 @@
+<?php include("header.php"); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+</head>
+<body>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Acer1.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Acer Nitro 5</h5>
+              <p class="card-text">Price: Rs.74,990</p>
+              <p class="card-text">Acer Nitro 5 11th Gen Intel Core i5-11400H 15.6 inches FHD 144Hz Gaming Laptop (8GB/512GB SSD/Windows 10/4 GB </p>
+              <button type="submit" name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Acer Nitro 5">
+              <input type="hidden" name="Price" value="74990">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Apple1.jpg" class="card-img-top" width="250" height="250" >
+            <div class="card-body text-center">
+              <h5 class="card-title">Macbook Air</h5>
+              <p class="card-text">Price: Rs.98,000</p>
+              <p class="card-text">2020 Apple MacBook Pro (13.3-inch/33.78 cm, Apple M1 chip with 8‑core CPU and 8‑core GPU, 8GB RAM, 256GB SSD)</p>
+              
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Macbook Air">
+              <input type="hidden" name="Price" value="98000">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Asus1.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">ASUS VivoBook 15</h5>
+              <p class="card-text">Price: Rs.59,800</p>
+              <p class="card-text">ASUS VivoBook 15 (2021), Intel Core i5-1135G7 11th Gen, 14-inch (35.56 cms) FHD Thin and Light Laptop /Intel Iris Xe</p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="ASUS VivoBook 15">
+              <input type="hidden" name="Price" value="59800">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Hp1.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">HP Pavilion 15</h5>
+              <p class="card-text">Price: Rs.68,490</p>
+              <p class="card-text">HP Pavilion 15,11th Gen Intel Core i5/FHD Thin and Light Laptop /Intel Iris Xe 16GB RAM/512GB SSD 14 inch(35.6 cm),FHD IPS Anti-Glare</p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="HP Pavilion 15">
+              <input type="hidden" name="Price" value="68490">
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </div>
+
+
+    <div class="row">
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Aspire 5.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Acer Aspire 5</h5>
+              <p class="card-text">Price: Rs.66,990</p>
+              <p class="card-text">Acer Aspire 5 Intel Core i5 11th Generation 14 inches Gaming Notebook Computer (8 GB/512 GB SSD/Windows 10 Home/Iris Xe Graphics </p>
+              <button type="submit" name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Acer Aspire 3">
+              <input type="hidden" name="Price" value="66900">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Aspire 3.jpg" class="card-img-top" width="250" height="250" >
+            <div class="card-body text-center">
+              <h5 class="card-title">Acer Aspire 3</h5>
+              <p class="card-text">Price: Rs.58,000</p>
+              <p class="card-text">Acer Aspire 3 AMD Ryzen 5 3500U Processor 15.6" (39.6 cms) Full HD Laptop - (8 GB/512 GB SSD/Windows 11 Home/AMD Radeon </p>
+              
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Acer Aspire 5">
+              <input type="hidden" name="Price" value="58000">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Dell Alienware.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Dell Alienware</h5>
+              <p class="card-text">Price: Rs.1,89,500</p>
+              <p class="card-text">Dell Alienware x15 R1 (2021) i7-11800H Gaming Laptop, 16GB, 1TB SSD, Win 11 + MSO'21, NVIDIA RTX 3060 6GB, 15.6"  </p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Dell Alienware">
+              <input type="hidden" name="Price" value="189500">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Dell New AMD 14.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Dell New AMD 14</h5>
+              <p class="card-text">Price: Rs.34,990</p>
+              <p class="card-text">Dell New 14 AMD Ryzen 3-3250U 14 inches FHD Display Laptop (8GB, 1TB HDD, Windows 11 + MS Office'21, Vega Graphics, Accent Black</p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Dell New AMD 14">
+              <input type="hidden" name="Price" value="34990">
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Dell Vestro 3510.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Dell Vestro 3510</h5>
+              <p class="card-text">Price: Rs.53,600</p>
+              <p class="card-text">Dell New 15 Intel i3-1115G4 15.6 inches FHD Display Laptop, 8GB, 256GB SSD, Windows 10 + MS Office, Integrated Graphics, Carbon Color </p>
+              <button type="submit" name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Dell Vestro 3510">
+              <input type="hidden" name="Price" value="53600">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Acer Aspire 7.jpg" class="card-img-top" width="250" height="250" >
+            <div class="card-body text-center">
+              <h5 class="card-title">Acer Aspire7</h5>
+              <p class="card-text">Price: Rs.64,870</p>
+              <p class="card-text">Acer Aspire7,Hybrid storage (SSD+HDD)Intel Core i5-1135G7 11th Gen, 14-inch (35.56 cms) FHD Thin and Light Laptop /Intel Iris Xe</p>
+              
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Acer Aspire 7">
+              <input type="hidden" name="Price" value="64870">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/Asus1.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">ASUS VivoBook 15</h5>
+              <p class="card-text">Price: Rs.59,800</p>
+              <p class="card-text">ASUS VivoBook 15 (2021), Intel Core i5-1135G7 11th Gen, 14-inch (35.56 cms) FHD Thin and Light Laptop /Intel Iris Xe</p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="ASUS VivoBook 15">
+              <input type="hidden" name="Price" value="59800">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-lg-3">
+        <form action="manage_cart.php" method="POST">
+          <div class="card">
+            <img src="images2/lenovo.jpg" class="card-img-top" width="250" height="250">
+            <div class="card-body text-center">
+              <h5 class="card-title">Lenovo Ideapad 15</h5>
+              <p class="card-text">Price: Rs.62,300</p>
+              <p class="card-text">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin & Light Laptop(16GB/512GB SSD/Windows 11/Office 2021/Backlit</p>
+              <button type="submit"  name="Add_To_Cart" class="btn btn-info">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Lenovo Ideapad 15">
+              <input type="hidden" name="Price" value="62300">
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </div>
+    
+  </div>
+</body>
+</html>
